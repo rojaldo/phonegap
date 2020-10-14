469be246608aa1f6ce1ff11493896ca2ad1f529d
Daniel Rojo <danielrojo@gmail.com>
2020-10-14 11:34:54 +0200

    Claculator running correctly

--- www/js/index.js
+++ www/js/index.js
@@ -41,8 +41,123 @@ var app = {
     }
 };
 
-function clickEvent(value) {
-    console.log('Entra en clickEvent');
+const state = {
+    init: 0,
+    firstFigure: 1,
+    secondFigure: 2,
+    result: 3
+}
+
+let currentState = state.init;
+
+let firstFigure = 0;
+let secondFigure = 0;
+let result = 0;
+let operator = '';
+
+function insertNumber(value) {
+    switch (currentState) {
+        case state.init:
+            firstFigure = value;
+            updateDisplay(value)
+            currentState = state.firstFigure;
+            break;
+        case state.firstFigure:
+            firstFigure = firstFigure * 10 + value;
+            updateDisplay(value)
+            break;
+        case state.secondFigure:
+            secondFigure = secondFigure * 10 + value;
+            updateDisplay(value)
+            break;
+        case state.result:
+            removeValues();
+            clearDisplay();
+            firstFigure = value;
+            updateDisplay(value);
+            currentState = state.firstFigure;
+            break;
+
+        default:
+            break;
+    }
+}
+
+function insertSymbol(value) {
+    switch (currentState) {
+        case state.init:
+
+            break;
+        case state.firstFigure:
+            if (isOpertor(value)) {
+                operator = value;
+                currentState = state.secondFigure;
+                updateDisplay(value)
+            }
+            break;
+        case state.secondFigure:
+            if (value === '=') {
+                doOperation();
+                updateDisplay(value + result)
+                currentState = state.result;
+            }
+            break;
+        case state.result:
+            const temp = result;
+            removeValues();
+            firstFigure = temp;
+            operator = value;
+            clearDisplay();
+            updateDisplay(firstFigure + value);
+            currentState = state.secondFigure;
+            break;
+
+        default:
+            break;
+
+    }
+}
+
+function updateDisplay(value) {
     let display = document.getElementById('display');
     display.innerHTML = display.innerHTML + value
+}
+
+function clearDisplay() {
+    let display = document.getElementById('display');
+    display.innerHTML = ''    
+}
+
+function removeValues() {
+    firstFigure = 0;
+    secondFigure = 0;
+    result = 0;
+    operator = '';
+}
+
+function doOperation() {
+    switch (operator) {
+        case '+':
+            result = firstFigure + secondFigure;
+            break;
+        case '-':
+            result = firstFigure - secondFigure;
+            break;
+        case '*':
+            result = firstFigure * secondFigure;
+            break;
+        case '/':
+            result = firstFigure / secondFigure;
+            break;
+        default:
+            break;
+    }
+}
+
+function isOpertor(value) {
+    if ((value === '+') || (value === '-') || (value === '*') || (value === '/')) {
+        return true
+    }
+
+    return false
 }
