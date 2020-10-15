4fdd36fc3a5785af8fdb86683f28c1dfd3101bc6
Daniel Rojo <danielrojo@gmail.com>
2020-10-15 12:30:16 +0200

    beers list with

--- www/pages/beers/beers.js
+++ www/pages/beers/beers.js
@@ -4,11 +4,27 @@ $(function () {
     $.get("https://api.punkapi.com/v2/beers", function (data) {
         console.log(data)
         beers = data;
+        showBeers = { ...beers };
+        filterBeers();
 
-        $.each(data, (beerIndex, beer) => { 
-            $("#beerslist").append('<li class="ui-first-child"><a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r">' + 
-            beerIndex + ' - ' + beer.name + ': ' + beer.abv + 'º' + '</a></li>'); 
-        });
     });
 
 });
+
+function checkCahnge() {
+    console.log('Cambio');
+
+     filterBeers()
+}
+
+function filterBeers() {
+    const min = $('#range-1a').val();
+    const max = $('#range-1b').val();
+    $("#beerslist").empty();
+    $.each(beers.filter((element) => {
+        return (element.abv >= min && element.abv <= max)
+    }), (beerIndex, beer) => {
+        $("#beerslist").append('<li class="ui-first-child"><a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r">' +
+            beerIndex + ' - ' + beer.name + ': ' + beer.abv + 'º' + '</a></li>');
+    });
+}
