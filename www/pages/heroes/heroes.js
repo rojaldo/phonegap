d4fd4d032f737bbeae4f697752ac09742f683285
Daniel Rojo <danielrojo@gmail.com>
2020-10-15 09:57:47 +0200

    hero list added

--- www/pages/heroes/heroes.js
+++ www/pages/heroes/heroes.js
@@ -1,1 +1,8 @@
-heroes = ['Superma', 'Spiderman', 'Batman']
+heroes = ['Superman', 'Spiderman', 'Batman'];
+
+$(function () {
+
+    heroes.forEach(element => {
+        $( '<li><a href="#">'+element+'</a></li>' ).appendTo( "#heroeslist" );
+    });
+});
