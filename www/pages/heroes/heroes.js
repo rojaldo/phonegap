a4e91d024d18514d7d3e0a0455bb204372ea716a
Daniel Rojo <danielrojo@gmail.com>
2020-10-15 10:32:58 +0200

    new hero to add

--- www/pages/heroes/heroes.js
+++ www/pages/heroes/heroes.js
@@ -3,6 +3,12 @@ heroes = ['Superman', 'Spiderman', 'Batman'];
 $(function () {
 
     heroes.forEach(element => {
-        $( '<li><a href="#">'+element+'</a></li>' ).appendTo( "#heroeslist" );
+        $('<li><a href="#">' + element + '</a></li>').appendTo("#heroeslist");
     });
 });
+
+function addHero() {
+    const newHero = $('#newhero').val();
+    heroes = [...heroes, newHero];
+    $("#heroeslist").append('<li class="ui-first-child"><a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r">' + newHero + '</a></li>');
+}
