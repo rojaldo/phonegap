6c06d81c2835ffb8f40e441566f59f112e847238
Daniel Rojo <danielrojo@gmail.com>
2020-10-15 11:37:25 +0200

    beers list added

--- www/pages/beers/beers.js
+++ www/pages/beers/beers.js
@@ -1,5 +1,14 @@
+let beers = [];
+
 $(function () {
     $.get("https://api.punkapi.com/v2/beers", function (data) {
         console.log(data)
+        beers = data;
+
+        $.each(data, (beerIndex, beer) => { 
+            $("#beerslist").append('<li class="ui-first-child"><a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r">' + 
+            beerIndex + ' - ' + beer.name + ': ' + beer.abv + 'º' + '</a></li>'); 
+        });
     });
+
 });
